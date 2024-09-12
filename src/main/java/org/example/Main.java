1b3793156147b5cbb7f7994b2b15578111cc5fea
David Chaves Rodríguez <19.dchaves@gmail.com>
2024-09-12 20:11:23 +0200

    Logic almost completed. Need to count ocurrences.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,24 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int numberOfAges;
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("How many ages are you telling me? ");
+        numberOfAges= myScanner.nextInt();
+        System.out.println("Ok, now tell me the " + numberOfAges + " ages and I will calculate how many candles you need: ");
+        for (int i = 0; i < numberOfAges; i++) {
+            int ageInput = myScanner.nextInt();
+            System.out.println(calculateCandles(ageInput));
+        }
+        }
+
+        public static String calculateCandles(Integer nextAge){
+            return Integer.toBinaryString(nextAge);
+
         }
     }
-}
